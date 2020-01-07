8754c8384231d8b6af5c71e1294a1b5537fcda31
Hesam Alavi <hesamalavi@Hesams-MacBook-Pro.local>
2020-01-07 15:52:51 +1100

    Third Commit

--- main.js
+++ main.js
@@ -3,47 +3,19 @@ const lastName = document.getElementById('LastName')
 const email = document.getElementById('EmailAddress')
 const password = document.getElementById('Password')
 const form = document.getElementById('form')
-// const errorElement1 = document.getElementById('error-FirstName')
-// const errorElement2 = document.getElementById('error-LastName')
-// const errorElement3 = document.getElementById('error-Email')
-// const errorElement4 = document.getElementById('error-Password')
-// const emailAddr = document.getElementById('EmailAddress')
+const errorEmail = document.getElementById('error-Email')
 
+// function to change the placeholder color when there is an error
 function update() {
  document.querySelector('input[type=text]').style.setProperty("--c", "red")
  document.querySelectorAll('input[type=text]')[1].style.setProperty("--c", "red")
- document.querySelectorAll('input[type=email]')[0].style.setProperty("--c", "red")
+ document.querySelectorAll('input[type=text]')[2].style.setProperty("--c", "red")
  document.querySelectorAll('input[type=password]')[0].style.setProperty("--c", "red")
 }
 
+// Form error messages when the fields are empty (after submitting)
 
-// document.getElementById("FirstName").addEventListener("input", clearError1);
-
-// function clearError1() {
-//  messages = [];
-//  errorElement1.innerHTML = "";
-// }
-// document.getElementById("LastName").addEventListener("input", clearError2);
-
-// function clearError2() {
-//  messages = [];
-//  errorElement2.innerHTML = "";
-// }
-// document.getElementById("EmailAddress").addEventListener("input", clearError3);
-
-// function clearError3() {
-//  messages = [];
-//  errorElement3.innerHTML = "";
-// }
-// document.getElementById("Password").addEventListener("input", clearError4);
-
-// function clearError4() {
-//  messages = [];
-//  errorElement4.innerHTML = "";
-// }
-
-
-
+// First Name error message
 form.addEventListener('submit', (e) => {
  let messages = []
  if (name.value === '' || name.value === null) {
@@ -53,29 +25,28 @@ form.addEventListener('submit', (e) => {
  if (messages.length > 0) {
   e.preventDefault()
   document.getElementById("FirstName").placeholder = messages.join(', ')
-  // document.querySelectorAll('[placeholder="First Name"]')
-  //  .style.color = "red";
  } else {
-  errorElement1.innerText = '';
+  document.getElementById("FirstName").placeholder = '';
  }
 })
 
+// Last Name error message
 form.addEventListener('submit', (e) => {
  let messages = []
  if (lastName.value === '' || lastName.value === null) {
   messages.push('Last Name Can\'t be empty')
  }
 
-
  if (messages.length > 0) {
   e.preventDefault()
   document.getElementById("LastName").placeholder = messages.join(', ')
  } else {
-  errorElement2.innerText = '';
+  document.getElementById("LastName").placeholder = '';
  }
 
 })
 
+// Email Address error message
 form.addEventListener('submit', (e) => {
  let messages = []
  if (email.value === '' || email.value === null) {
@@ -86,11 +57,12 @@ form.addEventListener('submit', (e) => {
   e.preventDefault()
   document.getElementById("EmailAddress").placeholder = messages.join(', ')
  } else {
-  errorElement3.innerText = '';
+  document.getElementById("EmailAddress").placeholder = '';
  }
 
 })
 
+// Last Name error message
 form.addEventListener('submit', (e) => {
  let messages = []
  if (password.value === '' || password.value === null) {
@@ -101,37 +73,26 @@ form.addEventListener('submit', (e) => {
   e.preventDefault()
   document.getElementById("Password").placeholder = messages.join(', ')
  } else {
-  errorElement4.innerText = '';
+  document.getElementById("Password").placeholder = '';
  }
 
 })
 
-
-
-
-
-
-
-
-
-
-
+// Email validation and error message
 function validateEmail(emailField) {
  var reg = /^([A-Za-z0-9_\-\.])+\@([A-Za-z0-9_\-\.])+\.([A-Za-z]{2,4})$/;
 
- if (reg.test(emailField.value) === false) {
-  errorElement3.innerText = 'Looks like this is not an email';
+ if (reg.test(emailField.value) == false) {
+  errorEmail.innerText = 'Looks like this is not an email';
   return false;
  }
- function clearEmailError() {
-  emailField = [];
-  errorElement3.innerHTML = "";
+ {
+
+  errorEmail.innerText = "";
   return true;
  }
-
-
-
 }
+
 
 
 
